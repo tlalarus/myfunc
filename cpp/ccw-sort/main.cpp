e5e4914847604843eff9f3e07131b69dc1c9cf33
minkyung <sim_mmmmk@golfzon.com>
2022-12-04 21:01:16 +0900

    좌표정렬 테스트코드 추가

---
+++ cpp/ccw-sort/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Pos{
+		int x;
+		int y;
+};
+
+class Node{
+	int index;
+	Pos pos;
+	vector<Node> children;
+	
+	insert();
+	
+};
+
+class Combination{
+		Node root;
+		
+		Combination(int n);
+};
+
+Combination::Combination(int n) {
+
+
+}
+void sorted_positions_combination(std::vector<Pos>& in_, std::vector<int>& indice_in_, std::vector<std::vector<Pos>>& out_, std::vector<int>& indice_out_){
+	
+	// 인덱스 초기화
+	
+	//
+	// 첫번째, 두번째 인덱스 지정
+	
+	//
+	
+	
+
+}
+int main() {
+	
+	// in : (u,v) 좌표들
+	// out : ccw 정렬된 (u,v) 좌표 조합들
+	
+	
+	return 0;
+}
